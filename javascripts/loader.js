00019593c1126da55f59272f21f2174771eae0ac
virtualchen <virtualchen@gmail.com>
2014-07-27 11:39:09 +0800

    backbone event

--- javascripts/loader.js
+++ javascripts/loader.js
@@ -1,13 +1,13 @@
 require.config({
 	baseUrl: 'bower_components/',
 	paths: {
-　　　　　　"jquery": 'jquery/dist/jquery',
-		  "underscore" :'underscore/underscore',
-		  "backbone": 'backbone/backbone'
-　　　　},
+		　　　　　　"jquery": 'jquery/dist/jquery',
+		"underscore" :'underscore/underscore',
+		"backbone": 'backbone/backbone'
+	　　　　},
 	shim:{
 		'backbone' :{
-				deps:['jquery','underscore']
+			deps:['jquery','underscore']
 		}
 	}
 
@@ -15,42 +15,24 @@ require.config({
 
 
 require([
-		'jquery',
-		'backbone',
-		'javascripts/main.js'		
+	'jquery',
+	'backbone',
+	'javascripts/main.js'		
 	] , function(){
+		console.log('load');
 		var app = app || {};
-		app.MessageView = Backbone.View.extend({
+		app.GameView = Backbone.View.extend({
+
+			el: '#gameBoard',
 			events:{
 				'click .grid': 'playRound'
 			}, 
-			playRound: function(){
-				 var grid = $(this);
-			 //console.log(this);
-    		 var gridIndex = grid.data('grid-no');
-    		 //console.log(gridIndex);
-    		 if(gridCount === 0 ){
-    		 	gameinfo[gridIndex] = 1;
-    		 	grid.html('X');
-    		 	gridCount ++ ;
-    		 }else{
-    		 	console.log(gridIndex);
-    		 	if(gameinfo[gridIndex] == 0){
-    		 		gridCount ++;
-    		 		if(gridCount %2 == 0){
-    		 			grid.html('O');
-    		 			gameinfo[gridIndex] =-1;
-    		 		}else{
-    		 			grid.html('X');
-    		 			gameinfo[gridIndex] = 1;
-    		 		}
-    		 		
-    		 	}
-    		 	checkWin();
 
-    		 }
-    		 
-			}
-		});
-});
+			playRound: function(){
+				console.log('click');
+			 
+    		}
+    	});
+    	app.gameView = new app.GameView();
+	});
 
--- javascripts/main.js
+++ javascripts/main.js
@@ -19,7 +19,7 @@ var checkWin = function(){
 		 board[5] = gameinfo[0] + gameinfo[1] + gameinfo[2];
 		 board[6] = gameinfo[0] + gameinfo[4] + gameinfo[8];
 		 board[7] = gameinfo[2] + gameinfo[4] + gameinfo[6];
-		 console.log(gameinfo);
+		 //console.log(gameinfo);
 	board.forEach(function(o){
 		//console.log(o);
 		if(o=== 3 || o === -3 || gridCount ===9){
@@ -40,10 +40,34 @@ var resetGame = function(){
 
 $().ready(function(){
 	gameInit();
-	// var obj = $(".grid");
-	// obj.each(function(){
-	// 	$(this).click(function(){
-			
-	// 	});
-	// });
+	var obj = $(".grid");
+	/*obj.each(function(){
+		$(this).click(function(){
+					var grid = $(this);
+				 //console.log(this);
+				 var gridIndex = grid.data('grid-no');
+	    		 //console.log(gridIndex);
+	    		 if(gridCount === 0 ){
+	    		 	gameinfo[gridIndex] = 1;
+	    		 	grid.html('X');
+	    		 	gridCount ++ ;
+	    		 }else{
+	    		 	//console.log(gridIndex);
+	    		 	if(gameinfo[gridIndex] == 0){
+	    		 		gridCount ++;
+	    		 		if(gridCount %2 == 0){
+	    		 			grid.html('O');
+	    		 			gameinfo[gridIndex] =-1;
+	    		 		}else{
+	    		 			grid.html('X');
+	    		 			gameinfo[gridIndex] = 1;
+	    		 		}
+	    		 		
+	    		 	}
+	    		 	checkWin();
+
+	    		 }
+	    	
+		});
+	});*/
 });
